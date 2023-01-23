1331c00c7125d48f4918d457937fb0355ab4f657
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2023-01-23 14:53:56 -0300

    1.0

---
+++ src/main/java/com/asfne/helpdesk/AsfneApplication.java
@@ -0,0 +1,13 @@
+package com.asfne.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsfneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsfneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asfne/helpdesk/AsfneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asfne.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsfneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
